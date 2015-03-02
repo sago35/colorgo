c84162f593370c25e306c872000c336220e69d7a
sago35 <sago35@gmail.com>
2015-03-02 21:14:00 +0900

    Add test and bench

---
+++ colorgo_test.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"github.com/codegangsta/cli"
+	"github.com/daviddengcn/go-colortext"
+	"testing"
+)
+
+func TestMakeColorRule(t *testing.T) {
+	{
+		cr, err := makeColorRule("", "red")
+		if err != nil {
+			t.Fatal("failed to call makeColorRule(): %s", err)
+		}
+		if cr.Regex.String() != "" {
+			t.Fatal("regex not matched")
+		}
+		if cr.Color != ct.Red {
+			t.Fatal("color not matched")
+		}
+	}
+
+	{
+		cr, err := makeColorRule("", "Red")
+		if err != nil {
+			t.Fatal("failed to call makeColorRule(): %s", err)
+		}
+		if cr.Regex.String() != "" {
+			t.Fatal("regex not matched")
+		}
+		if cr.Color != ct.Red {
+			t.Fatal("color not matched")
+		}
+	}
+
+	{
+		cr, err := makeColorRule("", "ReD")
+		if err != nil {
+			t.Fatal("failed to call makeColorRule(): %s", err)
+		}
+		if cr.Regex.String() != "" {
+			t.Fatal("regex not matched")
+		}
+		if cr.Color != ct.Red {
+			t.Fatal("color not matched")
+		}
+	}
+
+	{
+		cr, err := makeColorRule("", "RED")
+		if err != nil {
+			t.Fatal("failed to call makeColorRule(): %s", err)
+		}
+		if cr.Regex.String() != "" {
+			t.Fatal("regex not matched")
+		}
+		if cr.Color != ct.Red {
+			t.Fatal("color not matched")
+		}
+	}
+
+	cr2, err2 := makeColorRule("", "")
+	if err2 != nil {
+		t.Fatal("failed to call makeColorRule(): %s", err2)
+	}
+	if cr2.Color != ct.None {
+		t.Fatal("color not matched")
+	}
+
+}
+
+func BenchmarkColorize(b *testing.B) {
+	stdin := bytes.NewBufferString("foo\n")
+	stdout := new(bytes.Buffer)
+	set := flag.NewFlagSet("test", 0)
+	set.String("input", "utf8", "")
+	set.String("output", "utf8", "")
+	c := cli.NewContext(nil, set, set)
+
+	for i := 0; i < b.N; i++ {
+		Colorize(c, stdin, stdout)
+	}
+}
